ce5d4c1d06d99908190180c0b00d10602a95aa7d
oliga <oliga2505@mail.ru>
2022-12-25 14:10:15 -0500

    ContactPage created with changes

--- src/sunday/ContactPage.java
+++ src/sunday/ContactPage.java
@@ -4,5 +4,6 @@ public class ContactPage {
     public static void main(String[] args) {
 
         System.out.println("Add me into your ContactPage");
+        System.out.println("Merry Christmas!");
     }
 }
