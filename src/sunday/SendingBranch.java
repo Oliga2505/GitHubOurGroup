3506b0b78fff64a44e58eb8c73b8a6de5189cec3
oliga <oliga2505@mail.ru>
2022-12-25 13:50:02 -0500

    Sending this to Lilya

---
+++ src/sunday/SendingBranch.java
@@ -0,0 +1,12 @@
+package sunday;
+
+public class SendingBranch {
+
+    public static void main(String[] args) {
+
+
+        //Sending new branch to Lilya
+
+        System.out.println("Sending new branch");
+    }
+}
